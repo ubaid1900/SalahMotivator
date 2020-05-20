caa3356d1271a47d129d61b38886321b1ccab421
Ubaid Mohammed <ubaid1900@gmail.com>
2020-05-20 12:07:49 +0530

    tiny cleanup

--- src/app/CustomValidators.ts
+++ src/app/CustomValidators.ts
@@ -1,13 +1,24 @@
-import {ValidationErrors} from '@angular/forms';
+import { ValidationErrors, FormControl } from '@angular/forms';
 
 export class CustomValidators {
-  
-    static validateNumberOfAttachments(num: number): ValidationErrors {
-      const message = {
-        attachments: {
-          message: `Only ${num} attachments are allowed`
-        }
-      };
-      return message;
-    }
+
+  static validateNumberOfAttachments(num: number): ValidationErrors {
+    const message = {
+      attachments: {
+        message: `Only ${num} attachments are allowed`
+      }
+    };
+    return message;
   }
+
+  static noSpaces(num: number) {
+    return (c: FormControl) => {
+      const spacesRegex = `^\\s{${num},}$`;
+      const regex = new RegExp(spacesRegex);
+      const isJustSpaces = regex.test(c.value);
+      if (isJustSpaces) {
+        return { spaces: true };
+      }
+    };
+  }
+}
